3a626f5ac8444ef86fae7ac034fa4fbd19af082f
Dan Tong <dan@goldsky.com>
2022-12-30 11:30:37 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :slack do
-    get 'events/create'
+    resources :events, only: :create
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
