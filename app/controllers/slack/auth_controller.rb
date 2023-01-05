b1e16e261196c500907cb73ad74541ed539c5dfb
Dan Tong <dan@goldsky.com>
2023-01-04 19:59:02 -0500

    type

--- app/controllers/slack/auth_controller.rb
+++ app/controllers/slack/auth_controller.rb
@@ -1,7 +1,7 @@
 class Slack::AuthController < ApplicationController
   def index
     client = Slack::Web::Client.new
-    res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secrect: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
+    res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
     head :ok
   end
 end
