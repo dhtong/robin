5e80153303b9805a101e0d3b8e7fb6f68e1b2dc9
Dan Tong <dan@goldsky.com>
2023-01-04 20:09:06 -0500

    store token

--- app/controllers/slack/auth_controller.rb
+++ app/controllers/slack/auth_controller.rb
@@ -2,6 +2,15 @@ class Slack::AuthController < ApplicationController
   def index
     client = Slack::Web::Client.new
     res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
+    puts res 
+    ExternalAccount.create(customer: customer, token: res.access_token)
+
     head :ok
+  end
+
+  private
+
+  def customer
+    @customer ||= Customer.find_or_create_by(slack_team_id: params[:team_id][:id])
   end
 end
