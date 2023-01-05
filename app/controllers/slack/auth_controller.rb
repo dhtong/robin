4c527a7954482a81d117111536e38e77ba8b71eb
Dan Tong <dan@goldsky.com>
2023-01-04 20:14:24 -0500

    fixt

--- app/controllers/slack/auth_controller.rb
+++ app/controllers/slack/auth_controller.rb
@@ -3,14 +3,8 @@ class Slack::AuthController < ApplicationController
     client = Slack::Web::Client.new
     res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
     puts res 
+    customer = Customer.find_or_create_by(slack_team_id: res.team&.id)
     ExternalAccount.create(customer: customer, token: res.access_token)
-
     head :ok
-  end
-
-  private
-
-  def customer
-    @customer ||= Customer.find_or_create_by(slack_team_id: params[:team_id][:id])
   end
 end
