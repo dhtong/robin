12e14f532d835316bbb660616dd7d84f168eba66
Dan Tong <dan@supportbots.xyz>
2023-01-23 08:46:06 -0500

    debug response from pd

--- app/controllers/pagerduty/auth_controller.rb
+++ app/controllers/pagerduty/auth_controller.rb
@@ -3,6 +3,7 @@ class Pagerduty::AuthController < ApplicationController
     pd_client = Pagerduty::Client.new(ENV["PD_CLIENT_ID"], ENV["PD_CLIENT_SECRET"])
     # not handling response failure TODO
     resp = pd_client.oauth(params[:code]).body
+    p resp
     ExternalAccount.create(customer: customer, platform: "pagerduty", token: resp["access_token"], refresh_token: resp["refresh_token"])
     redirect_to "https://supportbots.xyz/", allow_other_host: true
   end
