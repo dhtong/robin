fde195800b8497c02355c319dedbad5edb46e3fa
Dan Tong <dan@supportbots.xyz>
2023-01-30 09:45:52 -0500

    get rid of message send in events controller

--- app/controllers/slack/events_controller.rb
+++ app/controllers/slack/events_controller.rb
@@ -11,7 +11,6 @@ class Slack::EventsController < ApplicationController
       Slack::RefreshHome.new(customer, @slack_client, params[:event][:user]).execute
     when 'app_mention'
       msg = record_message
-      # send_message
       Slack::PingOncall.perform_later(msg.id)
     end
     
@@ -22,30 +21,6 @@ class Slack::EventsController < ApplicationController
 
   def record_message
     Records::Message.create(content: params[:event][:text], customer: customer, channel_id: channel, event_payload: params[:event])
-  end
-
-  def send_message
-    return @slack_client.chat_postMessage(channel: channel, thread_ts: params[:event][:ts], text: "there is no oncall schedule linked to this channel yet.", as_user: true) if channel_config.blank?
-    oncall_users = channel_config.oncall_users
-    slack_users = oncall_users.map do |user|
-      begin
-        resp = @slack_client.users_lookupByEmail(email: user["email"])
-        resp["user"]["id"]
-      rescue Slack::Web::Api::Errors::UsersNotFound
-        @slack_client.chat_postMessage(channel: channel, thread_ts: params[:event][:thread_ts], text: "Slack user not found for #{user["email"]}", as_user: true)
-        nil
-      end
-    end.compact
-    return if slack_users.empty?
-
-    begin
-      @slack_client.conversations_invite(channel: channel, users: slack_users.join(","))
-    rescue Slack::Web::Api::Errors::AlreadyInChannel, Slack::Web::Api::Errors::MissingScope
-    end
-
-    mentions = slack_users.map{|u| "<@#{u}>"}
-
-    @slack_client.chat_postMessage(channel: channel, thread_ts: params[:event][:thread_ts], text: "Someone needs you! #{mentions.join(', ')}", as_user: true)
   end
 
   def channel
