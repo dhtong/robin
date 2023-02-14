8de25dc2a3380912b332691b6e2f065c29d99a78
Dan Tong <dan@supportbots.xyz>
2023-02-13 21:53:42 -0500

    db

--- db/migrate/20230212191735_create_user_contact_associations.rb
+++ db/migrate/20230212191735_create_user_contact_associations.rb
@@ -6,5 +6,6 @@ class CreateUserContactAssociations < ActiveRecord::Migration[7.0]
       t.string :status
       t.timestamps
     end
+    add_index :user_contact_associations, [:user_contact_id, :customer_user_id], unique: true, name: :idx_user_contact_assoc_unique
   end
 end
--- db/migrate/20230214024853_add_unique_to_user_contact_asso.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueToUserContactAsso < ActiveRecord::Migration[7.0]
-  def change
-    add_index :user_contact_associations, [:user_contact_id, :customer_user_id], unique: true, name: :idx_user_contact_assoc_unique
-  end
-end
