47882505af0191267d91a30fb623ce075d863e6c
Dan Tong <dan@supportbots.xyz>
2023-02-19 09:37:43 -0500

    fix id

--- app/controllers/slack/auth_controller.rb
+++ app/controllers/slack/auth_controller.rb
@@ -3,8 +3,8 @@ class Slack::AuthController < ApplicationController
     client = Slack::Web::Client.new
     res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
     customer = Records::Customer.find_or_create_by(slack_team_id: res.team&.id)
-    Records::CustomerUser.find_or_create_by(customer_id: customer.id, slack_user_id: res.authed_user.id)
-    customer.update(slack_access_token: res.access_token, installer_customer_user_id: res.authed_user.id)
+    installer = Records::CustomerUser.find_or_create_by(customer_id: customer.id, slack_user_id: res.authed_user.id)
+    customer.update(slack_access_token: res.access_token, installer_customer_user_id: installer.id)
     redirect_to "https://supportbots.xyz/", allow_other_host: true
   end
 end
