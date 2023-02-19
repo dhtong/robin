e9449882e91c8c8d564381bb3a94d19b7f6a3cd0
Dan Tong <dan@supportbots.xyz>
2023-02-19 08:56:56 -0500

    debug logging

--- app/controllers/slack/auth_controller.rb
+++ app/controllers/slack/auth_controller.rb
@@ -3,6 +3,7 @@ class Slack::AuthController < ApplicationController
     client = Slack::Web::Client.new
     res = client.oauth_v2_access(client_id: ENV["SLACK_CLIENT_ID"], client_secret: ENV["SLACK_CLIENT_SECRET"], code: params[:code])
     customer = Records::Customer.find_or_create_by(slack_team_id: res.team&.id)
+    p res.authed_user
     customer.update(slack_access_token: res.access_token)
     redirect_to "https://supportbots.xyz/", allow_other_host: true
   end
