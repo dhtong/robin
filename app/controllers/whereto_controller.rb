d9ba0bb1cb7a45e4f6e197255d90c7d6c8312e10
Dan Tong <dan@supportbots.xyz>
2023-06-17 17:58:57 -0400

    Fix response key lookup.

--- app/controllers/whereto_controller.rb
+++ app/controllers/whereto_controller.rb
@@ -10,8 +10,6 @@ class WheretoController < ApplicationController
     p params
     response_url = params[:response_url]
   
-    body = { text: "Try McDonald's!" }
-
     url = URI("https://api.foursquare.com/v3/places/search?query=restaurants&ll=40.733114%2C-73.955605&radius=500&fields=name%2Cwebsite&open_now=true&sort=RATING&limit=30")
     places = fetch_random_places_nearby
 
@@ -24,7 +22,8 @@ class WheretoController < ApplicationController
 
   def fetch_random_places_nearby(count: 3, lat: 40.719667, lon: -74.0002553)
     request_url = "https://api.foursquare.com/v3/places/search?query=restaurants&ll=#{lat}%2C#{lon}&radius=500&fields=name%2Cwebsite&open_now=true&sort=RATING&limit=30"
-    results = HTTParty.get(request_url, headers: {'Authorization': ENV["FS_API"]})["result"]
+    response = HTTParty.get(request_url, headers: {'Authorization': ENV["FS_API"]})
+    results = response["results"]
     
     results.map {|result| Place.new(**result)}
   end
