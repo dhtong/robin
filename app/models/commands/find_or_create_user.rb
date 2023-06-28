1ba4e195b3bbf32c79edfac01166f2e0fbc3824d
Dan Tong <dan@supportbots.xyz>
2023-06-28 09:37:31 -0400

    save first

--- app/models/commands/find_or_create_user.rb
+++ app/models/commands/find_or_create_user.rb
@@ -7,8 +7,9 @@ module Commands
       return if slack_user_id.nil? || slack_team_id.nil?
       c = Records::Customer.find_or_create_by(slack_team_id: slack_team_id)
       cu = Records::CustomerUser.find_or_initialize_by(slack_user_id: slack_user_id, customer: c)
-      ::PopulateCustomerUserData.perform_later(cu.id, referer_customer_id) if cu.new_record?
+      is_new_user = cu.new_record?
       cu.save!
+      ::PopulateCustomerUserData.perform_later(cu.id, referer_customer_id) if is_new_user
       cu
     end
   end
