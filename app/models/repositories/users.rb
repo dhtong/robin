a1d20e8459bbe1eff19c766624f65eb45469bd96
Dan Tong <dhtonge@gmail.com>
2023-08-22 14:44:22 -0400

    query

--- app/models/repositories/users.rb
+++ app/models/repositories/users.rb
@@ -11,7 +11,7 @@ module Repositories
     def slack_id_by_email(email:, customer:)
       cu = Records::CustomerUser.joins(:user_contacts).where(
         user_contacts: {number: email, method: :email},
-        customer_id: customer.id).limit(1)
+        customer_id: customer.id).limit(1).first
       return cu.slack_user_id if cu.present?
       @slack_client.new(token: customer.slack_access_token).users_lookupByEmail(email: email).dig("user", "id")
     end
